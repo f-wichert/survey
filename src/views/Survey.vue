5c443852de2efc9120e1292b2f51096c4d29d8aa
FreTD <frederickwichert@gmail.com>
2022-12-29 16:56:51 +0100

    Inital commit

---
+++ src/views/Survey.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>SurveyJS Library / Runner</h1>
+    <Survey :survey="survey" />
+  </div>
+</template>
+
+<script>
+import { Survey, Model, StylesManager } from "survey-vue";
+import "survey-vue/defaultV2.css";
+StylesManager.applyTheme("defaultV2");
+import { json } from "../data/survey_json";
+
+export default {
+  components: {
+    Survey
+  },
+  data() {
+    const model = new Model(json);
+    return {
+      survey: model
+    };
+  }
+};
+</script>
